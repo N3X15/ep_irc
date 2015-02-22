7086c10c83d399639c8878f6364a1ce94bfc4069
Rob Nelson <nexisentertainment@gmail.com>
2015-02-21 16:13:00 -0800

    Typo

--- static/js/postAceInit.js
+++ static/js/postAceInit.js
@@ -22,7 +22,8 @@ exports.postAceInit = function(hook, context){
 	if(!pad.getUserIsGuest()) {
 		params["nick"]=pad.getUserName();
 	}
-    var url= '<iframe src="http://'+clientVars.ep_irc_qchaturi+'?'+jQuery.params(params)+'" width="400" height=100% frameBorder=0></iframe>';
+	
+    var url= '<iframe src="http://'+clientVars.ep_irc_qchaturi+'?'+jQuery.param(params)+'" width="400" height=100% frameBorder=0></iframe>';
 
     $('#chatbox').html(url);
     $('#editorcontainer').css({"right":"406px", "width":"auto"});
